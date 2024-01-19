8c3582a7ed6ec96c9e594663ee252f98e4e00c5a
harrysonfmorais <harrysonfmorais@gmail.com>
2024-01-19 10:58:28 -0300

    feat:cart context and search page

--- src/app/(store)/search/page.tsx
+++ src/app/(store)/search/page.tsx
@@ -1,5 +1,72 @@
-export default async function Search() {
-  await new Promise((resolve) => setTimeout(resolve, 2000))
+import { api } from '@/data/api'
+import { Product } from '@/data/types/product'
+import Image from 'next/image'
+import Link from 'next/link'
+import { redirect } from 'next/navigation'
 
-  return <h1>Search</h1>
+interface SearchProps {
+  searchParams: {
+    q: string
+  }
+}
+
+async function searchProducts(query: string): Promise<Product[]> {
+  const response = await api(`/products/search?q=${query}`, {
+    // cache: 'no-store',
+    next: {
+      revalidate: 60 * 60, // 1min
+    },
+  })
+
+  const products = await response.json()
+  return products
+}
+
+export default async function Search({ searchParams }: SearchProps) {
+  const { q: query } = searchParams
+
+  if (!query) {
+    return redirect('/')
+  }
+
+  const products = await searchProducts(query)
+
+  return (
+    <div className="flex flex-col gap-4">
+      <p className="text-sm">
+        Resultados para: <span className="font-semibold">{query}</span>
+      </p>
+
+      <div className="grid grid-cols-3 gap-6">
+        {products.map((product) => {
+          return (
+            <Link
+              key={product.id}
+              href={`/product/${product.slug}`}
+              className="group relative flex items-center justify-center overflow-hidden rounded-lg bg-zinc-900"
+            >
+              <Image
+                className="transition-transform duration-500 group-hover:scale-105"
+                src={product.image}
+                width={480}
+                height={480}
+                quality={100}
+                alt=""
+              />
+
+              <div className="absolute bottom-28 right-28 flex h-12 max-w-[280px] items-center gap-2 rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+                <span className="truncate text-sm">{product.title}</span>
+                <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+                  {product.price.toLocaleString('pt-BR', {
+                    style: 'currency',
+                    currency: 'BRL',
+                  })}
+                </span>
+              </div>
+            </Link>
+          )
+        })}
+      </div>
+    </div>
+  )
 }
